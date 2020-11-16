449ea3d69bd31e53dbaa68a5ccfe00171db424bd
twist-tracer <twist.tracer@gmail.com>
2020-11-16 22:29:31 +0300

    Variables cast

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-person_name = 'Max'
-print(type(person_name))
+birthday_year = '1988'
+print(type(birthday_year))
 
-age = 30
-print(type(age))
-
-period = 3.2
+period = 20
 print(type(period))
 
-is_good = True
-print(type(is_good))
+age = int(birthday_year) + period
+print(age)
 
-person = None
-print(type(person))
+some_str = birthday_year + str(period)
+print(some_str)
