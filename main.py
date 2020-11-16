2fe83f23c20f1f00950a308df78d73f56d5ca519
twist-tracer <twist.tracer@gmail.com>
2020-11-16 22:22:01 +0300

    Variables

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print('Hello world!')
+name = 'Кузя'
+age = 1
+period = 2
+
+print('Попугайчик')
+print(name)
+print('в')
+print(age)
+print('года')
+print('научился говорить')
+print('свое имя')
+print(name)
+
+print()
+
+print('Через')
+print(period)
+print('года')
+print('Он научился говорить')
+print('Сколько ему лет')
+
+print()
+
+print(age + period)
