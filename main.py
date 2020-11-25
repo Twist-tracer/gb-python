2d387861692abdba84b44e66a1844dc93c38f2d2
twist-tracer <twist.tracer@gmail.com>
2020-11-25 20:41:31 +0300

    Hometask 2

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-number = int(input('Введите число: '))
+while True:
+    number = int(input('Введите число больше 0, но меньше 10: '))
+    if (number > 0) and (number < 10):
+        break
+    elif number <= 0:
+        print('Ваше число должно быть больше 0')
+    else:
+        print('Ваше число должно быть меньше 10')
 
-number += 2
 
-print('Ваше число + 2 будет ' + str(number))
+number = number ** 2
+
+print('Ваше число в квадрате будет ' + str(number))
