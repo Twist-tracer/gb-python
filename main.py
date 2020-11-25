654c7b7c6090b4b3a4fc4e0f19db8eb53da2f32f
twist-tracer <twist.tracer@gmail.com>
2020-11-25 21:42:17 +0300

    Fixes after check homework

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ lastname = input('Ваша фамилия: ')
 age = int(input('Ваш возраст: '))
 weight = int(input('Ваш вес: '))
 
-if (age < 30) and ((weight >= 50) and (weight < 120)):
+if (age <= 30) and ((weight >= 50) and (weight <= 120)):
     print(firstname, lastname + ',', age, 'год,', 'вес', weight, '- хорошее состояние')
+elif ((age > 30) and (age < 40)) and ((weight < 50) or (weight > 120)):
+    print(firstname, lastname + ',', age, 'год,', 'вес', weight, '- следует заняться собой')
 elif (age > 40) and ((weight < 50) or (weight > 120)):
     print(firstname, lastname + ',', age, 'год,', 'вес', weight, '- следует обратится к врачу!')
-elif (age > 30) and ((weight < 50) or (weight > 120)):
-    print(firstname, lastname + ',', age, 'год,', 'вес', weight, '- следует заняться собой')
 else:
     print('Пациент на мой вкус и полет фантазии')
