083f518efa283df038b52e69175d37f25e1e031a
twist-tracer <twist.tracer@gmail.com>
2020-11-30 22:46:10 +0300

    Hometask 4.1

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-firstname = input('Ваше имя: ')
-lastname = input('Ваша фамилия: ')
-age = int(input('Ваш возраст: '))
-weight = int(input('Ваш вес: '))
+my_list_1 = [2, 5, 8, 2, 12, 12, 4]
+my_list_2 = [2, 7, 12, 3]
 
-if (age <= 30) and ((weight >= 50) and (weight <= 120)):
-    print(firstname, lastname + ',', age, 'год,', 'вес', weight, '- хорошее состояние')
-elif ((age > 30) and (age < 40)) and ((weight < 50) or (weight > 120)):
-    print(firstname, lastname + ',', age, 'год,', 'вес', weight, '- следует заняться собой')
-elif (age > 40) and ((weight < 50) or (weight > 120)):
-    print(firstname, lastname + ',', age, 'год,', 'вес', weight, '- следует обратится к врачу!')
-else:
-    print('Пациент на мой вкус и полет фантазии')
+result = list(set(my_list_1) - set(my_list_2))
+
+print(result)
