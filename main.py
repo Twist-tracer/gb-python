e05fd13ea7eecd1a9bf7678da20d191b4be84990
twist-tracer <twist.tracer@gmail.com>
2020-12-01 22:58:39 +0300

    Hometask 6.1

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-my_list_1 = [2, 2, 5, 12, 8, 2, 12]
-my_set_1 = set(my_list_1)
+import random
 
-result = []
-for set_item in my_set_1:
-    set_items_from_list = []
+variant = None
+variants = ['<', '>', '=']
+min_number = 0
+max_number = 100
+number = random.randint(min_number, max_number)
 
-    for list_item in my_list_1:
-        if set_item == list_item:
-            set_items_from_list.append(list_item)
+while variant != '=':
+    print(f'Я думаю это число {number}, правильно?')
 
-    if len(set_items_from_list) == 1:
-        result.append(set_item)
+    print(
+        'Выберете один из вариантов ответа:',
+        '> - число больше',
+        '< - число меньше',
+        '= - правильно',
+        sep='\n'
+    )
 
+    variant = input('Ваш вариант: ')
+    while variant not in variants:
+        print('Введен некорректный вариант, попробуйте еще раз')
+        variant = input('Ваш вариант: ')
 
-print(result)
+    if variant == '<':
+        min_number = number
+    elif variant == '>':
+        max_number = number
+
+    number = int(min_number + ((max_number - min_number) / 2))
+else:
+    print('Ура!')
