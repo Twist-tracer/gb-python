15c4fef8e0920ed5c8f218467c75b3ab5edd0d6e
twist-tracer <twist.tracer@gmail.com>
2020-12-07 21:10:15 +0300

    Fixes after check hometask 10.1; 10.2; 10.3

--- fs_manager.py
+++ fs_manager.py
@@ -2,12 +2,12 @@ import os
 
 
 def create_dirs():
-    for i in range(1, 9):
+    for i in range(1, 10):
         os.mkdir('dir_{}'.format(i))
 
 
 def remove_dirs():
-    for i in range(1, 9):
+    for i in range(1, 10):
         os.rmdir('dir_{}'.format(i))
 
 # create_dirs()
