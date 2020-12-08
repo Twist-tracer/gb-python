d0d122c838ced400160050f5183a82138b56a3ca
twist-tracer <twist.tracer@gmail.com>
2020-12-08 21:56:41 +0300

    Hometask 14.1

---
+++ main.py
@@ -0,0 +1,6 @@
+fruits1 = ['orange', 'apple', 'banana']
+fruits2 = ['lemon', 'apple', 'mango']
+
+fruits_intersection = [fruit for fruit in fruits1 if fruit in fruits2]
+
+print(fruits_intersection)
