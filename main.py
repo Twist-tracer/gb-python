42fc381fb01d6f8be6690683aea41d696bf59795
twist-tracer <twist.tracer@gmail.com>
2020-12-08 22:11:01 +0300

    Hometask 14.3

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from math import sqrt
+
+
+def squares(items: list):
+    return [round(sqrt(item)) if item > 1 else item for item in items]
+
+
 numbers = [1, -4, 15, 17, 51, 24, 0, -32, 16, -17, -11]
 
-new_numbers = [number for number in numbers if ((number % 3) == 0) and (number > 0) and ((number % 4) != 0)]
-
-print(new_numbers)
+print(squares(numbers))
