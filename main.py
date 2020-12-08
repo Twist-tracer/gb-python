3c7d5bb7b461556ea814de5d77a38dca562a4e2b
twist-tracer <twist.tracer@gmail.com>
2020-12-08 22:20:55 +0300

    Hometask 14.4

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from math import sqrt
+def squares(number: int):
+    if number < 1 or number > 100:
+        raise ValueError('Введите число от 1 до 100')
+
+    if number == 13:
+        raise ValueError('Вы ввели запрещенное число!')
+
+    return number**2
 
 
-def squares(items: list):
-    return [round(sqrt(item)) if item > 1 else item for item in items]
+number = int(input('Введите число: '))
 
-
-numbers = [1, -4, 15, 17, 51, 24, 0, -32, 16, -17, -11]
-
-print(squares(numbers))
+try:
+    print(squares(number))
+except ValueError as e:
+    print(e)
