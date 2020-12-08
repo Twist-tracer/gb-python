00ede93b43c6a51b2a544fc95a3b1621ed3b9b44
twist-tracer <twist.tracer@gmail.com>
2020-12-08 22:00:28 +0300

    Hometask 14.2

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-fruits1 = ['orange', 'apple', 'banana']
-fruits2 = ['lemon', 'apple', 'mango']
+numbers = [1, -4, 15, 17, 51, 24, 0, -32, 16, -17, -11]
 
-fruits_intersection = [fruit for fruit in fruits1 if fruit in fruits2]
+new_numbers = [number for number in numbers if ((number % 3) == 0) and (number > 0) and ((number % 4) != 0)]
 
-print(fruits_intersection)
+print(new_numbers)
